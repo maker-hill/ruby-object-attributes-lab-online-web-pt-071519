075fad48791a8f9d63f855f62e584d21ea36204b
Jeremy Hill <j.c.hillmail@gmail.com>
2019-08-04 12:25:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def name=(dog_name)
+    name = dog_name
+    @name = name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end  
+  
+end   
